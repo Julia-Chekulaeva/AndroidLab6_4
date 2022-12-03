54a5d1d57a8466549ccaedbc481924f1dc92e0d4
Julia <julia4eck@gmail.com>
2022-12-04 02:52:59 +0300

    Initial commit

--- app/src/main/java/com/example/task2/MainActivity.kt
+++ app/src/main/java/com/example/task2/MainActivity.kt
@@ -4,23 +4,26 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
-    lateinit var button: Button
-    lateinit var image: ImageView
-    private val pictureUrl: String =
+
+    var pictureUrl: String =
         "https://www.sunhome.ru/i/wallpapers/190/igri-dlya-koshek-na-kompyutere.xxl.jpg"
+
+    lateinit var button: Button
+
+    lateinit var image: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val viewModel = MainActivityViewModel()
         button = findViewById(R.id.button)
         image = findViewById(R.id.imageView)
         button.setOnClickListener {
-            viewModel.load(pictureUrl)
-        }
-        viewModel.bitmap.observe(this) {
-            image.setImageBitmap(it)
+            Picasso.get()
+                .load(pictureUrl)
+                .into(image)
         }
     }
 }
